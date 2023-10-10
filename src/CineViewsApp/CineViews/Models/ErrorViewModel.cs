0ae989051c71e0b6e1c163730bc58816960496f8
dearaujofelipe <129342613+dearaujofelipe@users.noreply.github.com>
2023-10-10 16:58:49 -0300

    Delete src/CineViewsApp directory

--- src/CineViewsApp/CineViews/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CineViews.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
