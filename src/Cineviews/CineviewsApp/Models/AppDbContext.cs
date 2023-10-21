5fce5854e264dfb59caa2d527353d9adb09da18c
dearaujofelipe <mgdefelipe@outlook.com>
2023-10-21 12:36:22 -0300

    database criada

---
+++ src/Cineviews/CineviewsApp/Models/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CineviewsApp.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public  DbSet<Filme> Filmes { get; set; }
+
+    }
+}
--- src/Cineviews/CineviewsApp/Models/Filme.cs
+++ src/Cineviews/CineviewsApp/Models/Filme.cs
@@ -1,7 +1,24 @@
-﻿namespace CineviewsApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CineviewsApp.Models
 {
+    [Table("Filmes")]
     public class Filme
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage ="Informar o filme")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Informar o diretor")]
+        public string Diretor { get; set; }
+
+        [Required(ErrorMessage = "Informar a data")]
+        public int DataLancamento { get; set; }
+
+        [Required(ErrorMessage = "Informar o score")]
+        public int Score { get; set; }
     }
 }
