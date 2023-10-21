904ad5f786abc7602795672fdc0754b949657224
dearaujofelipe <mgdefelipe@outlook.com>
2023-10-21 17:10:47 -0300

    criptografia de senhas

--- src/Cineviews/CineviewsApp/Controllers/UsuariosController.cs
+++ src/Cineviews/CineviewsApp/Controllers/UsuariosController.cs
@@ -57,6 +57,7 @@ namespace CineviewsApp.Controllers
         {
             if (ModelState.IsValid)
             {
+                usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                 _context.Add(usuario);
                 await _context.SaveChangesAsync();
                 return RedirectToAction(nameof(Index));
@@ -96,6 +97,7 @@ namespace CineviewsApp.Controllers
             {
                 try
                 {
+                    usuario.Senha = BCrypt.Net.BCrypt.HashPassword(usuario.Senha);
                     _context.Update(usuario);
                     await _context.SaveChangesAsync();
                 }
