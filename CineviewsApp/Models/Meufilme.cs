24a4b45268dadd8ce0da75bd9513a7f91e98bbbf
dearaujofelipe <mgdefelipe@outlook.com>
2023-10-24 20:19:15 -0300

    test

---
+++ CineviewsApp/Models/Meufilme.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CineviewsApp.Models
+{
+    [Table("MeusFilmes")]
+    public class Meufilme
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="informar seu score")]
+        [Display(Name = "Meu Score")]
+        public int MeuScore { get; set; }
+
+        [Display(Name = "Filme")]
+        [Required(ErrorMessage = "informar o filme")]
+        public int FilmeId { get; set; }
+
+        [ForeignKey("FilmeId")]
+        public Filme Filme { get; set; }
+    }
+}
